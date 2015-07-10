cf691878c1a7826805d1987bdaecd4622ceaf901
jumaya <jumaya19@gmail.com>
2015-07-10 11:03:23 -0500

    migrate noticia

---
+++ database/migrations/2015_07_10_155821_create_noticia.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNoticia extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        //Migracion Para la Creacion de la Tabla Noticia
+
+        DB::statement('CREATE TABLE IF NOT EXISTS redbpim.noticia (
+                      id INT NOT NULL AUTO_INCREMENT,
+                      titulo_noticia VARCHAR(150) NOT NULL,
+                      cuerpo_noticia TEXT NOT NULL,
+                      PRIMARY KEY (id))
+                      ENGINE = InnoDB');
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        //Se Elimina la Tabla en Caso de Requerirse así
+
+        DB::statement('DROP TABLES redbpim.noticia') ;
+	}
+
+}
