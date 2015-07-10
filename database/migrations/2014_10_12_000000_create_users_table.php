9a22ffd90688a95e36fcbc2a64ea3b094e2edc8a
jumaya <jumaya19@gmail.com>
2015-07-10 10:49:18 -0500

    Merge branch 'master' of C:\xampp\htdocs\redbpim with conflicts.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,17 +11,18 @@ class CreateUsersTable extends Migration {
 	 * @return void
 	 */
 	public function up()
-	{
-		Schema::create('users', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->rememberToken();
-			$table->timestamps();
-		});
-	}
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+
 
 	/**
 	 * Reverse the migrations.
