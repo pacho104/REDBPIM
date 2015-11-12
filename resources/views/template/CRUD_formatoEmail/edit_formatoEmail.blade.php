deb8707809b4fff525d86f571e4fc66ac843dca7
pacho104 <pacho104@hotmail.com>
2015-11-12 16:55:22 -0500

    pacho 12/11/2015

---
+++ resources/views/template/CRUD_formatoEmail/edit_formatoEmail.blade.php
@@ -0,0 +1,42 @@
+@section('title'){{ 'Red BPIM - ' . Auth::user()->user_login }} @endsection
+@section('content')
+
+    @include('template.partials.logbar')
+
+    <div class="row-fluid">
+        @include('template.partials.error')
+        <div class="container">
+            <div class="jumbotrom">
+                <h3 class="text-center">
+                    Edición de Formato Email
+                </h3>
+            </div>
+        </div>
+        <br>
+
+        <div class="container">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                {!! Form::model($procesoBan,['route' => ['proceso.update',$procesoBan->id],'method'=>'PUT'])!!}
+                <fieldset>
+                    <div class="form-group">
+                        {!!Form::label('proceso_label','Nombre del proceso:',['class'=>'col-md-4 control-label']) !!}
+                        <div class="col-md-6">
+                            {!!Form::text('nombre_proceso',$procesoBan->nom_proceso,['class'=>'form-control','placeholder'=>'Ingrese
+                            el nombre'])!!}
+                        </div>
+                    </div>
+                    <br><br>
+                    {!!Form::button('Actualizar',['class'=>'btn btn-block
+                    btn-primary','data-toggle'=>'modal','data-target'=>'#myModal']) !!}
+
+                    @include('template.partials.confirmar')
+
+                </fieldset>
+                {!! Form::close() !!}
+
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+    </div>
+@stop
